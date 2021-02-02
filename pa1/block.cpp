9ed2ae101142b3d6ce7c886988fce07be2ad24c1
Mathew Balsdon <60245574+spreadnuts@users.noreply.github.com>
2021-02-02 10:30:51 -0800

    initial commit for pa1

--- pa1/block.cpp
+++ pa1/block.cpp
@@ -3,17 +3,37 @@
 #include <iostream>
 
 int Block::height() const {
-  /* your code here */
+  return data.back().size();
 }
 
 int Block::width() const {
-  /* your code here */
+  return data.size();
 }
 
 void Block::render(PNG &im, int x) const {
-  /* your code here */
+  vector<vector<HSLAPixel>> block = data;
+  // get columns from right to left
+  for (int i = x+data.size()-1; i > x-1; i--) {
+    vector<HSLAPixel> column = block.back();
+    block.pop_back();
+    // draw pixels from column (bottom to top)
+    for (int y = im.height()-1; y < -1; y--) {
+      // put a pixel from column on img
+      *im.getPixel(i, y) = column.back();
+      column.pop_back();
+    }
+  }
 }
 
 void Block::build(PNG &im, int x, int width) {
-  /* your code here */
+  
+  // push columns of pixels into data (left of img to right)
+  for (int i = x; i < x+width; i++) {
+    // gather pixels in a column (top of img to bottom)
+    vector<HSLAPixel> column;
+    for (unsigned y = 0; y < im.height(); y++) {
+    column.push_back(*im.getPixel(i, y));
+  }
+  data.push_back(column);
+  }
 }
