ed67af6e64adee9961d68d538678091368e1da9e
Mathew Balsdon <60245574+spreadnuts@users.noreply.github.com>
2021-03-06 13:44:38 -0800

    add tests for popR resizing

--- pa2/testStackQueue.cpp
+++ pa2/testStackQueue.cpp
@@ -41,7 +41,7 @@ TEST_CASE("queue::basic functions","[weight=1][part=queue]"){
 }
 
 TEST_CASE("deque::basic functions","[weight=1][part=deque]") {
-    /* pushR 10 popR 5 popL2 */
+    /* TEST: pushR 10 popR 5 popL 2 */
     Deque<int> intDeque0;
     vector<int> result0;
     vector<int> expected0;
@@ -92,7 +92,7 @@ TEST_CASE("deque::basic functions","[weight=1][part=deque]") {
     expected0.push_back(4);
     REQUIRE(result0 == expected0);
 
-    /* pushR 10 popR 10 */
+    /* TEST: pushR 10 popR 10 */
     Deque<int> intDeque1;
     vector<int> result1;
     vector<int> expected1;
@@ -112,4 +112,50 @@ TEST_CASE("deque::basic functions","[weight=1][part=deque]") {
     expected1.push_back(j);
     }
     REQUIRE(result1 == expected1);
+
+    /* TEST: pushR 10 popR 5 popL 2 popR 1 */
+    Deque<int> intDeque2;
+    vector<int> result2;
+    vector<int> expected2;
+    
+    /* pushR 10 */
+    cout << "peekR 10:";
+    for (int i = 0; i < 10; i++) {
+        intDeque2.pushR(i);
+        cout << intDeque2.peekR() << " ";
+    }
+    cout << endl;
+    /* popR 5 */
+        cout << "popR 5: ";
+    for (int j = 0; j < 5; j++) {
+        cout << intDeque2.popR() << " ";
+    }
+    cout << endl;
+    /* popL 2 */
+    cout << "popL 2: ";
+    for (int k = 0; k < 2; k++) {
+        cout << intDeque2.popL() << " ";
+    }
+    cout << endl;
+    /* popR */
+    cout << "popR: " << intDeque2.popR() << endl;
+
+    /* Expected: {2, 3} */
+    REQUIRE(intDeque2.size() == 2);
+    REQUIRE(intDeque2.isEmpty() == 0);
+    /* => {3} */
+    if (!intDeque2.isEmpty()) {
+        result2.push_back(intDeque2.popL());
+        REQUIRE(intDeque2.size() == 1);
+        REQUIRE(intDeque2.isEmpty() == 0);
+    }
+    /* => {} */
+    if (!intDeque2.isEmpty()) {
+        result2.push_back(intDeque2.popL());
+        REQUIRE(intDeque2.size() == 0);
+        REQUIRE(intDeque2.isEmpty() == 1);
+    }
+    expected2.push_back(2);
+    expected2.push_back(3);
+    REQUIRE(result2 == expected2);
 }
